f10860e1d44d52fba4ebe5766eb0ebba7bc0a53c
jvverde at win10 <jvverde@win10>
2020-04-14 13:12:26 +0100

    start table to show tasks

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding class="relative">
-    TASKS
+    TABLE
   </q-page>
 </template>
 
@@ -11,6 +11,9 @@ export default {
   name: 'Servers',
   data () {
     return {
+      task: [],
+      columns: [],
+      data: []
     }
   },
   watch: {
@@ -26,12 +29,28 @@ export default {
       if (match) {
         match.forEach(m => {
           console.log('Match', m)
-          task.get(m, 'LIST', true)
+          task.get(m, 'CSV', true)
             .then((result) => {
-              const lines = (result.split(/[\n\r]+/) || []).filter(e => e).map(e => {
-                const match = e.match(/(?<name>.+?):\s*(?<value>.+)$/)
-                return match ? match.groups : {}
+              const lines = (result.split(/[\n\r]+/) || []).filter(e => e)
+              // .filter(e => e).map(e => {
+              //  const match = e.match(/(?<name>.+?):\s*(?<value>.+)$/)
+              //  return match ? match.groups : {}
+              // })
+              const headers = lines.shift().split(/","/)
+              headers.forEach(h => {
+                this.columns.push({ name: h, label: h, field: h })
               })
+              lines.forEach(line => {
+                const values = line.split(/","/)
+                const data = {}
+                for (const i in values) {
+                  data[headers[i]] = values[i]
+                }
+                this.data.push(data)
+                console.log(this.columns)
+                console.log(this.data)
+              })
+              this.task.push(lines)
               console.log('Lines:', lines)
             })
         })
