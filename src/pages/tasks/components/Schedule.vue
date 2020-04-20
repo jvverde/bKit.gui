92159f4f59e5b5ac4676d52393614ceffc51dca5
jvverde at win10 <jvverde@win10>
2020-04-20 20:53:37 +0100

    split code

---
+++ src/pages/tasks/components/Schedule.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="column no-wrap items-center">
+    <div class="q-mt-lg q-gutter-sm row items-center no-wrap">
+      <q-input
+        v-model.number="_freq"
+        maxlength="2"
+        outlined
+        dense
+        label="Every"
+        style="max-width:7em;min-width:5em"
+        type="number"/>
+      <q-option-group
+        v-model="_period"
+        :options="periods"
+        color="positive"
+        inline
+        dense
+      />
+    </div>
+    <div class="q-mt-lg q-gutter-sm row items-center no-wrap">
+      <span>Start at</span>
+      <q-input v-model="_start" dense outlined type="time"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'schedule',
+  props: {
+    freq: {
+      type: Number,
+      default: 1
+    },
+    period: {
+      type: String
+    },
+    periods: {
+      type: Array,
+      default: () => []
+    },
+    start: {
+      type: String
+    }
+  },
+  computed: {
+    _freq: {
+      get () { return this.freq },
+      set (val) { return this.$emit('update:freq', val) }
+    },
+    _period: {
+      get () { return this.period },
+      set (val) { return this.$emit('update:period', val) }
+    },
+    _start: {
+      get () { return this.start },
+      set (val) { return this.$emit('update:start', val) }
+    }
+  }
+}
+</script>
---
+++ src/pages/tasks/components/Taskname.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="column no-wrap items-center">
+    <q-input
+      class="q-mt-lg"
+      v-model="value"
+      prefix="BKIT-"
+      outlined
+      dense
+      label="Taskname"
+      hint="Choose a name not yet listed"
+      @keyup.enter="check"
+      @keydown.tab="check"
+      @blur="check"
+      @keydown="valid = invalid = false"
+      type="text">
+      <template v-slot:append>
+        <q-btn flat no-caps stack round
+          icon="add"
+          @click="check"
+          size="xs" color="positive"/>
+      </template>
+      <template v-slot:after>
+        <q-icon  v-if="invalid" name="warning" flat color="negative"/>
+        <q-icon  v-else-if="valid" name="done" flat color="positive"/>
+      </template>
+    </q-input>
+  </div>
+</template>
+
+<script>
+
+const task = require('windows-scheduler')
+
+export default {
+  name: 'taskname',
+  data () {
+    return {
+      value: '',
+      invalid: false,
+      valid: false
+    }
+  },
+  props: ['name'],
+  methods: {
+    check () {
+      task.get('BKIT-' + this.value, 'LIST')
+        .then(list => {
+          console.log('Tasks exists', list)
+          this.invalid = true
+        }).catch(e => {
+          this.invalid = false
+          this.valid = true
+          this.$emit('update:name', this.value)
+        })
+    }
+  }
+}
+</script>
