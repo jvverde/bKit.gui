60a74717629332286836fc039fdeb89f43d0d917
jvverde at win10 <jvverde@win10>
2020-04-27 16:04:14 +0100

    new and much better way to comput ancestors

--- src/pages/tasks/components/__tests__/Tree_jest.spec.js
+++ src/pages/tasks/components/__tests__/Tree_jest.spec.js
@@ -33,26 +33,34 @@ describe('Tree.vue', () => {
     expect(wrapper.name()).toBe('tree')
   })
 
-  const makeAncestorsOrSelf = (string, sep = '/') => {
-    if (!string) return []
-    return string.split('/').reduce((result, value) => { // Create a list of ancestors paths
-      const last = result[result.length - 1]
-      const next = last ? [last, value].join(path.sep) : value
-      return next ? [...result, next] : result
-    }, [])
+  const makeAncestors = (pathstr) => {
+    pathstr = path.normalize(pathstr)
+    if (!pathstr || pathstr === path.sep) return []
+    const re = new RegExp(`(.+)\\${path.sep}[^\\${path.sep}]+\$`)
+    const re1 = new RegExp(`\\${path.sep}\$`)
+    const ancestors = []
+    let str = pathstr.replace(re1,'')
+    let result
+    while ( (result = re.exec(str)) !== null) {
+      str = result[1] // Parent is on first group
+      ancestors.push(str)
+    }
+    if (ancestors.length === 0 && pathstr.startsWith(path.sep)) ancestors.push(path.sep)
+    return ancestors
   }
 
   const paths = [
-    ...['c:','c:/','c:/a','c:/a/b','c:/a/b/c','c:/a/b/c/','a','a/b','a/b/','a/b//','/','/a','/a/b'].map(e => {
-      const r = makeAncestorsOrSelf(e) 
-      return [r.pop(), r.reverse()]
+    ...['c:','c:/','c:/a','c:/a/b','c:/a/b/c','c:/a/b/c/','a','a/b','a/b/','a/b//','/','/a','/a/','/a/b','','//','///'].map(e => {
+      const self = path.normalize(e) 
+      const r = makeAncestors(self)
+      return [self, r]
     }),
     ['', []],
     [null, []],
     [undefined, []]
   ]
 
-  it.each(paths)('Expected ancestors of %s to be %s', (path, ancestors) => {
+  it.each(paths)('Ancestors of %s must be: %s', (path, ancestors) => {
     const wrapper = mountwith(path)
     expect(wrapper.vm.ancestors).toEqual(ancestors)
   })
