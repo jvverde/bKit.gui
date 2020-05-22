7a9233dad7bf62b681da41fb098d7621c0982584
jvverde at win10 <jvverde@win10>
2020-05-22 23:17:20 +0100

    setup

--- src/pages/Update.vue
+++ src/pages/Update.vue
@@ -1,11 +1,53 @@
 <template>
-  <q-page padding class="relative">
-    <div>bKitPath: {{bKitPath}}</div>
-    <q-btn icon="system_update_alt" label="Clone" @click="clone" :loading="loading"/>
-    <q-btn icon="folder" label="New bkit Location" @click="chosebkitLocation"/>
-    <q-btn icon="folder" label="Install" @click="install"/>
-    <div v-if="!bkitinstalled">bKit client is not installed on this location. I must clone it from repository</div>
-    <div v-else>bKit client is installed</div>
+  <q-page padding class="q-pa-lg column relative fit">
+    <div class="q-mb-xl self-center">This app is a GUI for the bKit Client.</div>
+    <div>
+      <span>Here we may change the bKit Client location, download or update it from the public repository</span>
+      <span v-if="isWin">and install cygwin and packages as well other underlying apps</span>
+    </div>
+    <div>
+      We may do it manually one by one or just let it install
+      <q-btn color="ok" flat dense label="automatically" no-caps @click="setup"/>
+    </div>
+    <q-list padding class="absolute-center">
+      <q-item>
+        <q-item-section side>
+          <q-item-label>Locatiom:</q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{location}}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-btn color="ok" flat label="Change" no-caps @click="chosebkitLocation"/>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section side>
+          <q-item-label>Bkit Client is present:</q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{bkitinstalled}}</q-item-label>
+        </q-item-section>
+        <q-item-section side v-if="bkitinstalled">
+          <q-btn color="ok" flat label="Update" no-caps @click="pull"/>
+        </q-item-section>
+        <q-item-section side v-else>
+          <q-btn color="ok" flat label="Download" no-caps @click="clone"/>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section side>
+          <q-item-label>Bkit Client is installed:</q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{bkitok}}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-btn v-if="bkitok" flat color="ok" label="Reinstall" no-caps @click="install"/>
+          <q-btn v-else color="ok" flat label="Install" no-caps @click="install"/>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -22,6 +64,7 @@ export default {
   name: 'Update',
   data () {
     return {
+      step: 1,
       isWin,
       loading: false
     }
@@ -31,6 +74,9 @@ export default {
     bKitPath: {
       get () { return this.bkitlocation },
       set (val) { this.setbkitLocation(val) }
+    },
+    location () {
+      return this.bKitPath || path.normalize(path.join(app.getAppPath(), '../'))
     },
     needCheck () {
       return this.bkitinstalled ? this.bkitlocation : false
@@ -49,9 +95,8 @@ export default {
   methods: {
     ...mapMutations('global', ['setbkitLocation', 'checkbkitInstalled', 'checkbkitOk']),
     chosebkitLocation () {
-      const dst = this.bKitPath || path.normalize(path.join(app.getAppPath(), '../'))
+      const dst = this.location
       console.log('dst', dst)
-      let location = null
       dialog.showOpenDialog({
         title: 'Select a new location for bKit client',
         defaultPath: dst,
@@ -59,7 +104,7 @@ export default {
         properties: ['openDirectory', 'promptToCreate']
       }).then((result) => {
         if (result.filePaths instanceof Array) {
-          location = result.filePaths[0]
+          const location = result.filePaths[0]
           if (location !== null) {
             this.bKitPath = location
           }
@@ -104,6 +149,9 @@ export default {
       if (0 | code === 0) {
         this.checkbkitOk()
       }
+    },
+    setup () {
+      // if (!bkitinstalled)
     }
   }
 }
