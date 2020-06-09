b8c39a19d1f4660f8eabb91c6e3e2bc73908e1b2
Jorge Monteiro <jorgemonteiro2013@gmail.com>
2020-06-09 18:10:22 +0100

    picker

--- src/pages/Jorge.vue
+++ src/pages/Jorge.vue
@@ -1,13 +1,17 @@
 <template>
   <q-page padding class="relative">
-    Customize .....
+  <Photoshop v-model="colors" v-if="chooseop === 'Photoshop'"/>
+  <Material v-model="colors" v-if="chooseop === 'Material'"/>
+  <Chrome v-model="colors" v-if="chooseop === 'Chrome'"/>
+  <Compact v-model="colors" v-if="chooseop === 'Compact'"/>
+  <Sketch v-model="colors" v-if="chooseop === 'Sketch'"/>
+  <Slider v-model="colors" v-if="chooseop === 'Slider'"/>
+   Customize .....
+    <q-select v-model="chooseop" :options="choose" label="Escolha" />
     <q-select v-model="cor" :options="options" label="Nome" />
-
-    Valor: <q-input filled v-model="valor" label="Valor em RGB" />
-
     <q-btn color="primary" label="change" @click="change"/>
     <q-badge color="ok">texto do ok</q-badge>
-    <q-badge color="error">texto do error</q-badge>
+    <q-badge color="error">texto do error</q-badge>>
   </q-page>
 </template>
 
@@ -16,20 +20,32 @@
 // import fs from 'fs'
 
 import { colors } from 'quasar'
+import { Photoshop, Material, Chrome, Compact, Sketch, Slider } from 'vue-color'
 
 export default {
   name: 'Customize',
   data () {
     return {
+      colors: '',
       valor: '',
-      cor: '',
-      options: ['ok', 'error']
+      cor: 'ok',
+      options: ['ok', 'error'],
+      chooseop: '',
+      choose: ['Chrome', 'Compact', 'Material', 'Photoshop', 'Sketch', 'Slider']
     }
+  },
+  components: {
+    Photoshop: Photoshop,
+    Material: Material,
+    Chrome: Chrome,
+    Compact: Compact,
+    Sketch: Sketch,
+    Slider: Slider
   },
   methods: {
     change () {
-      colors.setBrand(this.cor, this.valor)
-      console.log(colors)
+      console.log('colors', this.colors)
+      colors.setBrand(this.cor, this.colors.hex)
     }
   },
   async mounted () {
