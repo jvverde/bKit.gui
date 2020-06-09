01b1d003763a55a27e0f38b3c0eadfa3cba99451
Serprest Generic Account <serprest@colibri.serprest.pt>
2020-06-09 15:50:49 +0100

    jorge

---
+++ src/pages/Jorge.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page padding class="relative">
+    Customize .....
+    <q-select v-model="cor" :options="options" label="Nome" />
+
+    Valor: <q-input filled v-model="valor" label="Valor em RGB" />
+
+    <q-btn color="primary" label="change" @click="change"/>
+    <q-badge color="ok">texto do ok</q-badge>
+    <q-badge color="error">texto do error</q-badge>
+  </q-page>
+</template>
+
+<script>
+
+// import fs from 'fs'
+
+import { colors } from 'quasar'
+
+export default {
+  name: 'Customize',
+  data () {
+    return {
+      valor: '',
+      cor: '',
+      options: ['ok', 'error']
+    }
+  },
+  methods: {
+    change () {
+      colors.setBrand(this.cor, this.valor)
+      console.log(colors)
+    }
+  },
+  async mounted () {
+  }
+}
+</script>
