78b8842c44c98e8be7d1f89de893d5ad853d9b50
jvverde at win10 <jvverde@win10>
2020-06-12 15:57:24 +0100

    more code split

---
+++ src-electron/main-process/statics.js
@@ -0,0 +1,9 @@
+/**
+ * Set `__statics` path to static files in production;
+ * The reason we are setting it here is that the path needs to be evaluated at runtime
+ */
+if (process.env.PROD) {
+  global.__statics = path.join(__dirname, 'statics').replace(/\\/g, '\\\\')
+}
+
+export default __statics
