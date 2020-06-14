24dc1a370002f15fdb0b689ddc44232a03617ea7
jvverde at win10 <jvverde@win10>
2020-06-14 21:51:17 +0100

    some cleaning

--- bkit-client/lib/getUSER.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -u
-declare -r sdir=$(dirname -- "$(readlink -en -- "$0")")	#Full sdir
-source "$sdir/functions/all.sh"
-
-echo "$BKITUSER"
--- bkit-client/setup/subinacl.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-setlocal
-set "OP=%~dp0"
-if not exist "%OP%..\3rd-party\subinacl\" mkdir "%OP%..\3rd-party\subinacl\"
-copy /b /v /y "%OP%subinacl\subinacl.exe" "%OP%..\3rd-party\subinacl"
-endlocal
