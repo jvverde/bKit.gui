74dfff042e18d77d4366f360001b3bece965489b
Isidro <jvverde@win11>
2021-09-29 20:04:09 +0100

    simplify ListAccounts.vue

--- src/pages/servers/ListAccounts.vue
+++ src/pages/servers/ListAccounts.vue
@@ -6,14 +6,14 @@
       </div>
       <div v-for="(account, index) in accounts" :key="index">
         <q-chip clickable
-          @click="load(account)"
+          @click="changeTo(account)"
           :color="color(account)"
-          :outline="isCurrent(account)"
+          :outline="isSelected(account)"
           icon="person">
           {{account.user}}
         </q-chip>
       </div>
-      <div v-if="noChildren && zero" class="absolute-center column items-center q-gutter-x-sm z-top">
+      <div v-if="noAccounts && zero" class="absolute-center column items-center q-gutter-x-sm z-top">
         <div class="text-h6">No accounts</div>
         <div>You don't have any account configured for server {{server}}</div>
         <div>Please add a new one</div>
@@ -40,15 +40,15 @@ const compbyuser = (a, b) => {
   return 0
 }
 
-const findNewElePosition = (a, b) => {
-  // find the position of the first of new elements
-  // assuming a.length > b.length
-  for (let i = 0; i < b.length; i++) {
-    const ae = a[i]
-    if (!b.find(be => be.user === ae.user && be.servername === ae.servername)) return i
-  }
-  return b.length
-}
+// const findNewElePosition = (a, b) => {
+//   // find the position of the first of new elements
+//   // assuming a.length > b.length
+//   for (let i = 0; i < b.length; i++) {
+//     const ae = a[i]
+//     if (!b.find(be => be.user === ae.user && be.serverURL === ae.serverURL)) return i
+//   }
+//   return b.length
+// }
 
 import { mapGetters, mapActions } from 'vuex'
 
@@ -64,14 +64,20 @@ export default {
     ...mapGetters('accounts', ['getAccountsByServerURL', 'currentAccount']),
     selected: {
       get () {
-        return this.currentOf[this.server]
+        // return this.currentOf[this.server]
+        const { server, user } = this.$route.params
+        return this.accounts.find(a => a.serverURL === server && a.user === user)
       },
-      set (val) {
-        this.$set(this.currentOf, this.server, val)
+      set (account) {
+        this.$router.push({ name: 'Account', params: { server: account.serverURL, user: account.user } }).catch(() => {})
+        // this.$set(this.currentOf, this.server, val)
       }
     },
     accounts () {
       return [...this.getAccountsByServerURL(this.server)].sort(compbyuser)
+    },
+    accountNames () {
+      return this.accounts.map(a => a.name)
     },
     naccounts () {
       console.log('accounts', this.accounts)
@@ -86,61 +92,29 @@ export default {
     //     servername: this.$route.params.server
     //   } : {}
     // },
-    noChildren () { return this.$route.name === 'ListAccounts' }
+    noAccounts () { return this.$route.name === 'ListAccounts' }
   },
   props: ['server'],
   watch: {
-    server: {
-      immediate: true,
-      handler (servername, old) {
-        this.selectOne()
-      }
-    },
-    $route: {
-      immediate: true,
-      handler (to, from) {
-        console.log('Watch $route', to.name)
-        if (to && to.params && to.name === 'Account') {
-          this.selected = this.accounts.find(a => a.serverURL === to.params.server && a.user === to.params.user)
-        }
-      }
-    },
-    accounts: {
-      immediate: false,
-      deep: true,
-      handler (accounts, old) {
-        console.log('Watch Accounts')
-        // Here we are only interested on changes in the number of accounts (new or removed) under same server
-        if (old && old.length && accounts && accounts.length && accounts[0].serverURL !== old[0].serverURL) {
-          console.log('Do nothing when change servername')
-        } else if (accounts && old && accounts.length !== old.length) {
-          const selected = this.selected || {}
-          setTimeout(() => { // wait a while and let any pending transition to occur first
-            console.log('Process Watch')
-            if (accounts.length === 0) {
-              console.log('There is no more accounts')
-              this.$router.push({ name: 'ListAccounts', params: { server: this.server } }).catch(() => {})
-            } else if (accounts.length > old.length) {
-              console.log('Detected a new account')
-              const index = findNewElePosition(accounts, old)
-              console.log('The new element is at position', index)
-              this.load(accounts[index])
-            } else if (accounts.length < old.length) {
-              console.log('Detected a possible removed account')
-              const index = old.findIndex(a => a.user === selected.user && a.serverURL === selected.serverURL)
-              if (index === -1) {
-                console.log('Not found the removed account. Do nothing')
-              } else if (index >= accounts.length) {
-                console.log('Set to last account')
-                this.load(accounts[accounts.length - 1])
-              } else {
-                console.log('Set to account at same position')
-                this.load(accounts[index])
-              }
-            }
-          }, 100)
-        } // fi
-      }
+    // server: {
+    //   immediate: true,
+    //   handler (servername, old) {
+    //     this.selectOne()
+    //   }
+    // },
+    // $route: {
+    //   immediate: true,
+    //   handler (to, from) {
+    //     console.log('Watch $route', to.name)
+    //     if (to && to.params && to.name === 'Account') {
+    //       this.selected = this.accounts.find(a => a.serverURL === to.params.server && a.user === to.params.user)
+    //     }
+    //   }
+    // },
+    accounts (accounts) {
+      console.log('Watch Accounts')
+      if (this.selected && this.accountNames.includes(this.selected.name)) return // Do nothing if selected account is already included in the accounts list
+      this.change2current() // Otherwise changeTo to current account
     }
   },
   methods: {
@@ -148,32 +122,48 @@ export default {
     add () {
       this.$router.push({ name: 'NewAccount', params: { server: this.server } })
     },
-    isCurrent (account) {
-      return account && this.currentAccount && account.name === this.currentAccount.name
+    isSelected (account) {
+      return account && this.selected && account.name === this.selected.name
     },
     color (account) {
-      return this.isCurrent(account) ? 'active' : ''
+      return this.isSelected(account) ? 'active' : ''
     },
-    load (account) {
-      if (!account || !account.serverURL || !account.user) return
-      this.$router.push({ name: 'Account', params: { server: account.serverURL, user: account.user } }).catch(() => {})
-    },
-    async selectOne () {
-      console.log('SelectOne')
-      if (this.selected && this.accounts.find(a => a.name === this.selected.name)) {
-        console.log('Show previous one')
-        this.load(this.selected)
+    // load (account) {
+    //   if (!account || !account.serverURL || !account.user) return
+    //   this.$router.push({ name: 'Account', params: { server: account.serverURL, user: account.user } }).catch(() => {})
+    // },
+    changeTo (account) {
+      if (this.accountNames.includes(account.name)) {
+        this.selected = account
       } else {
-        const currentAccount = await this.getCurrentAccount()
-        if (currentAccount && currentAccount.serverURL === this.server) {
-          console.log('Go to current Server')
-          this.load(currentAccount)
-        } else {
-          console.log('Go to first account')
-          this.load(this.accounts[0])
-        }
+        this.selected = this.accounts[0]
+      }
+    },
+    async change2current () {
+      const current = await this.getCurrentAccount()
+      if (current) {
+        this.changeTo(current)
+      } else { // Else go to selected Account whichever it is
+        this.changeTo(this.selected)
       }
     }
+    // ,
+    // async selectOne () {
+    //   console.log('SelectOne')
+    //   if (this.selected && this.accounts.find(a => a.name === this.selected.name)) {
+    //     console.log('Show previous one')
+    //     this.load(this.selected)
+    //   } else {
+    //     const currentAccount = await this.getCurrentAccount()
+    //     if (currentAccount && currentAccount.serverURL === this.server) {
+    //       console.log('Go to current Server')
+    //       this.load(currentAccount)
+    //     } else {
+    //       console.log('Go to first account')
+    //       this.load(this.accounts[0])
+    //     }
+    //   }
+    // }
   },
   async mounted () {
   },
--- src/pages/servers/index.vue
+++ src/pages/servers/index.vue
@@ -3,8 +3,8 @@
     <div v-if="hasServers"
       class="q-pa-sm q-mt-sm  q-gutter-x-sm row items-center full-width self-start">
       <div>Manage server:</div>
-      <div @click="change(serverURL)"
-        v-for="(serverURL, index) in servernames" :key="index">
+      <div @click="changeTo(serverURL)"
+        v-for="(serverURL, index) in serversList" :key="index">
         <q-btn flat :color="color(serverURL)" icon="storage" :icon-right="icon(serverURL)" no-caps>
           <span style="color:black">{{serverURL}}</span>
         </q-btn>
@@ -14,21 +14,15 @@
       </div>
     </div>
     <div v-else-if="noServers" class="fit column items-center justify-center">
-      <q-btn v-show="!loading" rounded icon="add" label="Add a server" no-caps size="xl" @click="add"/>
+      <q-btn rounded icon="add" label="Add a server" no-caps size="xl" @click="add"/>
     </div>
     <div class="fit relative-position routerview">
       <router-view></router-view>
     </div>
-    <q-inner-loading :showing="loading">
-      <q-spinner-ios size="xl" color="loader"/>
-      <span v-if="msg">{{msg}}</span>
-    </q-inner-loading>
   </q-page>
 </template>
 
 <script>
-// import { getAccounts } from 'src/helpers/credentials'
-// import { catched } from 'src/helpers/notify'
 import { mapActions, mapGetters } from 'vuex'
 let lastServer
 
@@ -36,9 +30,6 @@ export default {
   name: 'Servers',
   data () {
     return {
-      msg: undefined,
-      adding: false,
-      error: false
     }
   },
   computed: {
@@ -46,10 +37,9 @@ export default {
     servers () {
       return [...new Set(this.getAccounts.map(a => a.serverURL))]
     },
-    loading () { return this.msg && this.msg.length > 0 },
     hasServers () { return this.servers.length > 0 },
     noServers () { return !this.hasServers && this.$route.name === 'servers' },
-    servernames () { return [...this.servers].sort() },
+    serversList () { return [...this.servers].sort() },
     selectedServer: {
       get () {
         return this.$route.params.server
@@ -63,9 +53,9 @@ export default {
     selectedServer (serverURL) {
       lastServer = serverURL
     },
-    servernames (names) {
-      if (this.selectedServer && names.includes(this.selectedServer)) return // Do nothing id selected server is already included in the servers list
-      this.change2current() // Otherwise change to current server
+    servers (names) {
+      if (this.selectedServer && names.includes(this.selectedServer)) return // Do nothing if selected server is already included in the servers list
+      this.change2current() // Otherwise changeTo to current server
     }
   },
   methods: {
@@ -82,20 +72,19 @@ export default {
     icon (serverURL) {
       return this.mystyle(serverURL).icon
     },
-    change (serverURL) {
+    changeTo (serverURL) {
       if (this.servers.includes(serverURL)) {
         this.selectedServer = serverURL
       } else {
-        // go to first server if serverURl is not in servers list
         this.selectedServer = this.server[0]
       }
     },
     async change2current () {
       const current = await this.getCurrentAccount()
       if (current) {
-        this.change(current.serverURL)
+        this.changeTo(current.serverURL)
       } else { // Else go to selected server whichever it is
-        this.change(this.selectedServer)
+        this.changeTo(this.selectedServer)
       }
     },
     add () {
@@ -106,7 +95,7 @@ export default {
     console.log('Last server was', lastServer)
     if (this.selectedServer) return // do nothing if a server is already selected on route path
     if (lastServer) { // Use last server to avoid annoying the user experience
-      this.change(lastServer)
+      this.changeTo(lastServer)
     } else {
       this.change2current()
     }
