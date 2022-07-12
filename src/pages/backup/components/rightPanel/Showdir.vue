33b4297783c635d6fbeda00ce73b8eb316bdfeae
jvv <jvverde@outlook.com>
2022-07-12 22:07:23 +0100

    write .rsync-filter files

--- src/pages/backup/components/rightPanel/Showdir.vue
+++ src/pages/backup/components/rightPanel/Showdir.vue
@@ -9,7 +9,9 @@
       <q-btn color="path" flat size="md" icon="navigate_before" @click="goBack" :disable="!canGoBack"/>
       <q-btn color="path" flat size="md" icon="navigate_next" @click="goForward" :disable="!canGoForward"/>
       <q-btn color="path" flat size="md" icon="arrow_upward" @click="goUp"/>
+      <div style="margin-left:auto"/>
       <q-btn v-show="!empty" round color="button" flat size="sm" icon="cloud_circle" @click="toggle"/>
+      <q-btn round color="warning" flat size="sm" icon="do_not_disturb_on" @click="ban"/>
     </div>
     <div v-show="!done" class="loader">
       <q-spinner-ios size="100px" color="loader"/>
@@ -19,6 +21,7 @@
 <script>
 
 import compare from 'src/helpers/compare'
+import { writeFile } from 'src/helpers/writefs'
 import dir from 'src/mixins/directory'
 import { mapGetters, mapMutations } from 'vuex'
 
@@ -37,7 +40,11 @@ export default {
   },
   methods: {
     ...mapMutations('backups', ['show', 'hide', 'toggle']),
-    ...mapMutations('view', ['goUp', 'goBack', 'goForward'])
+    ...mapMutations('view', ['goUp', 'goBack', 'goForward']),
+    ban () {
+      console.log('Ban', this.fullpath)
+      writeFile(`${this.fullpath}/.rsync-filter`, '- /*\n')
+    }
   },
   beforeDestroy () {
     // console.log('Destroy ShowDir')
