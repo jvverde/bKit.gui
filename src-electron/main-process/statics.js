e1b595aa721e493778c3e76f2d768a565d65a5c8
jvv <jvverde@outlook.com>
2022-08-10 15:07:05 +0100

    __statics

--- src-electron/main-process/statics.js
+++ src-electron/main-process/statics.js
@@ -2,13 +2,15 @@
  * Set `__statics` path to static files in production;
  * The reason we are setting it here is that the path needs to be evaluated at runtime
  */
-import path from 'path'
 
-console.log('__statics', __statics)
-console.log('__dirname', __dirname)
+import say from './say'
+import { join } from 'path'
 
 if (process.env.PROD) {
-  global.__statics = path.join(__dirname, 'statics').replace(/\\/g, '\\\\')
+  global.__statics = join(__dirname, 'statics').replace(/\\/g, '\\\\')
 }
+
+say.log('__statics', __statics)
+say.log('__dirname', __dirname)
 
 export default __statics
